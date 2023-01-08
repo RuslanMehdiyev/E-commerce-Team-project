14345e944154ff495cca76fb07e7a89c2f1d7d28
Elgun <elgunnb@code.edu.az>
2023-01-08 16:06:07 +0400

    latest

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,9 @@
-import DoubleCard from "../components/DoubleCard"
-import Brands from "../components/Brands"
-import Testimonials from "../components/Testimonials"
-import Quality from "../components/Quality"
-import Offers from "../components/Offers"
+
 
 function Home() {
   return (
     <div>
-      <Offers/>
-      <Quality/>
-      <Testimonials/>
-      <Brands/> 
-      <DoubleCard/>
+      
     </div>
   )
 }
